ef93ce38e0cf2d4e5dceb73ef166870498a2fd94
Andweki <andweki@gmail.com>
2026-05-18 14:37:36 +0300

    first commit

---
+++ datatypes.py
@@ -0,0 +1,35 @@
+#Data types define the type of data that a variable can hold. In Python, there are several built-in data types, including:
+# 1. Integer (int): Represents whole numbers.
+age=25
+print (age)
+
+# 2. Float (float): Represents decimal numbers.
+price=19.99
+print (price)
+
+# 3. String (str): Represents a sequence of characters.
+language="Python"
+print (language)
+
+student_name="Alice"
+print (student_name)
+
+# 4. Boolean (bool): Represents True or False values.
+is_student=True
+print (is_student)
+
+# 5. List: Represents an ordered collection of items.
+fruits=["apple", "banana", "orange"]
+print (fruits)
+
+# 6. Tuple: Represents an ordered, immutable collection of items.
+coordinates=(10, 20)
+print (coordinates)
+
+# 7. Dictionary (dict): Represents a collection of key-value pairs.
+person={"name": "John", "age": 25}
+print (person)
+
+# 8. Set: Represents an unordered collection of unique items.
+unique_numbers={1, 2, 3, 4, 5}
+print (unique_numbers)
---
+++ variables.py
@@ -0,0 +1,11 @@
+name="John"
+print (name)
+
+age=25
+print (age)
+
+Location= "Nairobi"
+print (Location)
+
+full_name="John Doe"
+print (full_name)
