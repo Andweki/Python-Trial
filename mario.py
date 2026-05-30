29c3b55331844fce6bc4046b0fc44dfce6454ee0
Andweki <andweki@gmail.com>
2026-05-30 21:11:29 +0300

    mario

---
+++ mario.py
@@ -0,0 +1,16 @@
+def main ():
+    print_square(3)
+
+#2 dimensional printing
+def print_square (size):
+    #for each row in square
+    for i in range(size):
+        #for each brick in row
+        #for j in range(size):
+            #print brick
+            print ("#" * size)
+
+
+main()
+
+        
